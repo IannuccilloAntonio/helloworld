2e7dbff63e022912379be4b0764369562ea3dcde
Iannuccillo <iannuccillo.antonio99@gmail.com>
2017-02-03 18:32:48 +0100

    va bene?

---
+++ primogrado.py
@@ -0,0 +1,17 @@
+print("Programma che calcola le equazioni di primo grado ax * b = 0")
+a= input("Inserisci il valore di a ")
+b = input("inserisci il valore di b ")
+
+if a!=0 and b!=0 : 
+	print "il risultato e' ", -b/a
+
+elif a==0 and b==0 : 
+	print "Il risultato e' 0"
+
+elif  b==0 : 
+	print "il risultato e' sempre ", -b/a
+
+else : 
+	print "l'equazione e' impossibile "
+
+
