c5986ae0e2dd4249cf3bdc9df897ee88f03598b1
Antonio Iannuccillo <iannuccillo.antonio99@gmail.com>
2017-02-06 19:28:51 +0100

    Update primogrado.py
    Va bene?

--- primogrado.py
+++ primogrado.py
@@ -3,9 +3,9 @@ a= input("Inserisci il valore di a ")
 b = input("inserisci il valore di b ")
 
 if a == 0 and b == 0: 
-	print "il risultato e' indeterminato "
+	print "il risultato dell'equazione e' indeterminato "
 elif a == 0: 
-	print "Il risultato e' impossibile "
+	print "Il risultato dell'equazione e' impossibile "
 
 else: 
-	
+	print "Il risultato dell'equazione e' ", -b/a
