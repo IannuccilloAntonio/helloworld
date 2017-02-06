73e5f3eaa35d9dfb45d10c6d5f41a645cfb80699
Antonio Iannuccillo <iannuccillo.antonio99@gmail.com>
2017-02-06 16:38:34 +0100

    Update volume.py
    Modificato

--- volume.py
+++ volume.py
@@ -1,7 +1,8 @@
 import math
 x = input("Vuoi calcolare il volume del cubo - 1, o della sfera - 2? ")
+if x == 1:
 l = input("Inserisci il lato ")
-if x==1 :
    print "il volume del cubo e' ", l**3
-else : 
-   print "il volume della sfera e' ", (4.0*math.pi*l**3)/3.0
+else: 
+   r = input("Inserisci il valore del raggio ")
+   print "il volume della sfera e' ", (4.0*math.pi*r**3)/3.0
