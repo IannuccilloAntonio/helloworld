9f352741214806af021f5b7dfc253dc80d5f7adb
Antonio Iannuccillo <iannuccillo.antonio99@gmail.com>
2017-02-06 19:24:08 +0100

    Update primogrado.py

--- primogrado.py
+++ primogrado.py
@@ -2,13 +2,10 @@ print("Programma che calcola le equazioni di primo grado ax * b = 0")
 a= input("Inserisci il valore di a ")
 b = input("inserisci il valore di b ")
 
-if a != 0 and b != 0: 
-	print "il risultato e' ", -b/a
-
-elif a == 0 and b == 0: 
-	print "Il risultato e' 0"
+if a == 0 and b == 0: 
+	print "il risultato e' indeterminato "
+elif a == 0: 
+	print "Il risultato e' impossibile "
 
 else: 
-	print "l'equazione e' impossibile "
-
-
+	
