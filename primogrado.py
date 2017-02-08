b95bb54512c129bb4a45f56e1619291952b0e899
alunno <alunno@lab03-06>
2017-02-08 11:34:15 +0100

    modificato

--- primogrado.py
+++ primogrado.py
@@ -1,5 +1,5 @@
 print("Programma che calcola le equazioni di primo grado ax * b = 0")
-a= input("Inserisci il valore di a ")
+a = input("Inserisci il valore di a ")
 b = input("inserisci il valore di b ")
 
 if a == 0 and b == 0: 
@@ -8,4 +8,4 @@ elif a == 0:
 	print "Il risultato dell'equazione e' impossibile "
 
 else: 
-	print "Il risultato dell'equazione e' ", -b/a
+	print "Il risultato dell'equazione e' ",float(-b)/float(a)
