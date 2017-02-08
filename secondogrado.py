15dd486d3bdfc925448f9efb411cb65c27b5e2b7
alunno <alunno@lab03-06>
2017-02-08 11:41:49 +0100

    va bene?

--- secondogrado.py
+++ secondogrado.py
@@ -9,16 +9,16 @@ if a == 0 and b == 0 and c == 0:
 elif a == 0 and b == 0:
         print "L'equazione e' impossibile "
 elif a == 0:
-        x= -c/b
+        x= float(-c)/float(b)
 	print "x vale ", x
 else: 
-	delta = (b**2) - (4*a*c)
+	delta = (b**2) - (4.0*a*c)
 	if delta > 0: 
-    		x1 =  ((-b) + math.sqrt(delta))/2*a
-		x2 =  ((-b) - math.sqrt(delta))/2*a
+    		x1 =  (float(-b) + math.sqrt(delta))/2.0*a
+		x2 =  (float(-b) - math.sqrt(delta))/2.0*a
 		print "x1 vale ", x1 , "x2 vale ", x2
 	elif delta == 0:
-		x = -b/(a*2)
+		x = -b/(a*2.0)
 		print "x vale = ", x
 	else:
 		print "L'equazione non ammette soluzioni "
